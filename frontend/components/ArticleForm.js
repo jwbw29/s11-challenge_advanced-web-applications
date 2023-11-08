17b3a96dfcd7f44013d5bde17db55c278b428638
Justin Byrd <justinbyrd7@gmail.com>
2023-11-08 09:33:12 -0600

    cancelEdit

--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import PT from 'prop-types'
+import React, { useEffect, useState } from "react";
+import PT from "prop-types";
 
-const initialFormValues = { title: '', text: '', topic: '' }
+const initialFormValues = { title: "", text: "", topic: "" };
 
 export default function ArticleForm(props) {
-  const [values, setValues] = useState(initialFormValues)
+  const [values, setValues] = useState(initialFormValues);
   // ✨ where are my props? Destructure them here
 
   useEffect(() => {
@@ -12,32 +12,33 @@ export default function ArticleForm(props) {
     // Every time the `currentArticle` prop changes, we should check it for truthiness:
     // if it's truthy, we should set its title, text and topic into the corresponding
     // values of the form. If it's not, we should reset the form back to initial values.
-  })
+  });
 
-  const onChange = evt => {
-    const { id, value } = evt.target
-    setValues({ ...values, [id]: value })
-  }
+  const onChange = (evt) => {
+    const { id, value } = evt.target;
+    setValues({ ...values, [id]: value });
+  };
 
-  const onSubmit = evt => {
-    evt.preventDefault()
+  const onSubmit = (evt) => {
+    evt.preventDefault();
     // ✨ implement
     // We must submit a new post or update an existing one,
     // depending on the truthyness of the `currentArticle` prop.
-  }
+  };
 
   const isDisabled = () => {
     // ✨ implement
     // Make sure the inputs have some values
-        /**
+    /**
      * username.trim().length >= 3 && password.trim().length >= 8 ? false : true
      */
-    return true
-  }
+    return true;
+  };
 
   const cancelEdit = () => {
     // [ ] Reset the form to initial values on click
-  }
+    setValues(initialFormValues);
+  };
 
   return (
     // [ ] ✨ fix the JSX: make the heading display either "Edit" or "Create"
@@ -65,11 +66,13 @@ export default function ArticleForm(props) {
         <option value="Node">Node</option>
       </select>
       <div className="button-group">
-        <button disabled={isDisabled()} id="submitArticle">Submit</button>
-        <button onClick={Function.prototype}>Cancel edit</button>
+        <button disabled={isDisabled()} id="submitArticle">
+          Submit
+        </button>
+        <button onClick={cancelEdit}>Cancel edit</button>
       </div>
     </form>
-  )
+  );
 }
 
 // 🔥 No touchy: LoginForm expects the following props exactly:
@@ -77,10 +80,11 @@ ArticleForm.propTypes = {
   postArticle: PT.func.isRequired,
   updateArticle: PT.func.isRequired,
   setCurrentArticleId: PT.func.isRequired,
-  currentArticle: PT.shape({ // can be null or undefined, meaning "create" mode (as opposed to "update")
+  currentArticle: PT.shape({
+    // can be null or undefined, meaning "create" mode (as opposed to "update")
     article_id: PT.number.isRequired,
     title: PT.string.isRequired,
     text: PT.string.isRequired,
     topic: PT.string.isRequired,
-  })
-}
+  }),
+};
