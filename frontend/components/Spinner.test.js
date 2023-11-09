07c01a3e0f448f39aff629a24f57dd966235ff5a
Justin Byrd <justinbyrd7@gmail.com>
2023-11-09 08:24:03 -0600

    working on spinner test

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -2,8 +2,19 @@
 // that it renders what it should for the different props it can take.--only prop it takes is on={SpinnerOn}
 
 import React from "react";
+import { render } from "@testing-library/react";
 import Spinner from "./Spinner";
 
-test("Spinner loads when it should", () => {
-  expect(true).toBe(false);
+describe("Spinner", () => {
+  // test that when on is false, Spinner renders null
+  it("renders null when on is false", () => {
+    const { container } = render(<Spinner on={false} />);
+    expect(container.firstChild).toBeNull();
+  });
+
+  // test that when on is true, Spinner renders the correct elements
+  it("renders the correct elements when on is true", () => {
+    const { container } = render(<Spinner on={false} />);
+    expect(container.firstChild).not.toBeNull();
+  });
 });
