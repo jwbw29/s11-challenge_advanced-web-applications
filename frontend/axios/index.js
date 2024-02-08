d71514b469d056053131c255df992d51a0f5b85c
Justin Byrd <justinbyrd7@gmail.com>
2024-02-08 16:20:44 -0600

    Refactor axiosWithAuth to use dynamic baseURL

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -3,9 +3,13 @@ import axios from "axios";
 
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
+  const baseURL =
+    process.env.NODE_ENV === "production"
+      ? "/api"
+      : "http://localhost:9000/api";
 
   return axios.create({
-    baseURL: "http://localhost:9000/api",
+    baseURL,
     headers: {
       authorization: token,
     },
